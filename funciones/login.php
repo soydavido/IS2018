561348c53854cea78852dfc59001b976661eac47
luisalreves <44936136+luisalreves@users.noreply.github.com>
2018-11-18 13:29:03 -0400

    Update login.php

--- funciones/login.php
+++ funciones/login.php
@@ -51,7 +51,7 @@ pg_close($canal);
 
   <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="index.php">Stellar</a>
+        <a class="navbar-brand" href="IS2018/index.php">Stellar</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
